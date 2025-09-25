70adc7be5016ffb6192e13af238d6438ecc80ba5
Developer-gpk <dev.goplek@gmail.com>
2025-09-25 10:19:27 -0600

    Working on seo card

--- apps/web/pages/_app.jsx
+++ apps/web/pages/_app.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import '../design/styles/base.scss'
+import Head from "next/head";
 
 export default function MyApp({ Component, pageProps, router }) {
 
     return(
         <>
+            <Head>
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+            </Head>
             <Component {...pageProps} key={router.asPath} />
         </>
     )
