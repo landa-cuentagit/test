fc92e8a6f11b9131df4fa6b6f751184428dc2ac0
Developer-gpk <dev.goplek@gmail.com>
2025-09-25 10:05:37 -0600

    Working on seo card

--- apps/web/template/Template.jsx
+++ apps/web/template/Template.jsx
@@ -3,55 +3,52 @@ import Head from 'next/head'
 
 const Template = ({ children, title, description, keywords, template }) => {
 
+    const siteUrl = "https://landa-site.netlify.app/";
+    const img = "https://landa-site.netlify.app/social.png"; // asegúrate 1200x630, PNG/JPG, pública
+
     return (
         <>
-            <Head>
-                <meta charSet="UTF-8"/>
-                <meta httpEquiv="X-UA-Compatible" content="IE=edge"/>
-                <meta name="viewport" content="width=device-width, initial-scale=1"/>
+    <Head>
+      <meta charSet="UTF-8" />
+      <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-                <title>{title}</title> 
-                <meta name="description" content={description} />
-                <meta name="keywords" content={keywords} />
-                <meta name="author" content="Goplek" />
-                {/*}<meta name="robots" content="INDEX, FOLLOW, ARCHIVE" />{*/}
+      <title>{title}</title>
+      <meta name="description" content={description} />
+      {keywords && <meta name="keywords" content={keywords} />}
+      <meta name="author" content="Goplek" />
+      {/* <meta name="robots" content="index, follow" /> */}
 
-                <link rel="alternate" hrefLang="es" href='' />
+      {/* Evita href vacío; si no tienes alternates aún, quita esta línea */}
+      {/* <link rel="alternate" hrefLang="es" href={siteUrl} /> */}
 
-                <link rel="shortcut icon" href="https://landa-site.netlify.app/favicon.png" type="image/x-icon" />
-                <link rel="apple-touch-icon" sizes="72x72" href="https://landa-site.netlify.app/favicon.png" />
-                <link rel="apple-touch-icon" sizes="114x114" href="https://landa-site.netlify.app/favicon.png" />
+      <link rel="icon" href={`${siteUrl}favicon.png`} />
+      <link rel="apple-touch-icon" sizes="180x180" href={`${siteUrl}favicon.png`} />
 
-                <meta itemProp="name" content={title}/>
-                <meta itemProp="description" content={description}/>
-                <meta itemProp="url" content='https://landa-site.netlify.app/' />
-                <meta itemProp="image" content='https://landa-site.netlify.app/social.png'/>
+      {/* Open Graph */}
+      <meta property="og:type" content="website" />
+      <meta property="og:site_name" content="Landa" />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      <meta property="og:url" content={siteUrl} />
+      <meta property="og:image" content={img} />
+      <meta property="og:image:secure_url" content={img} />
+      <meta property="og:image:type" content="image/png" />
+      <meta property="og:image:width" content="1200" />
+      <meta property="og:image:height" content="630" />
+      <meta property="og:image:alt" content="Landa" />
+      {/* opcional: <meta property="og:locale" content="es_ES" /> */}
 
-                <meta property="og:type" content="website"/>
-                <meta property="fb:app_id" content="app-id"/>
-                <meta property="og:site_name" content={title}/>
-                <meta property="og:title" content={title}/>
-                <meta property="og:description" content={description}/>
-                <meta property="og:url" content='https://landa-site.netlify.app/' />
-                <meta property="og:image" content='https://landa-site.netlify.app/social.png' />
-
-                {/* Asegúrate que esta imagen exista y cumpla 1200x630 */}
-                <meta property="og:image:secure_url" content="https://landa-site.netlify.app/social.png" />
-                <meta property="og:image:type" content="image/png" />
-                <meta property="og:image:width" content="1200" />
-                <meta property="og:image:height" content="630" />
-                <meta property="og:image:alt" content="Landa" />
-
-                <meta name="twitter:site" content={title}/>
-                <meta name="twitter:title" content={title}/>
-                <meta name="twitter:description" content={description}/>
-                <meta name="twitter:url" content='https://landa-site.netlify.app/' />
-                <meta name="twitter:image" content='https://landa-site.netlify.app/social.png' />
-                <meta name="twitter:card" content="summary"/>
-
-                <meta name="theme-color" content="#F1EDE5" />
-                <meta name="apple-mobile-web-app-status-bar-style" content="#F1EDE5" />
-            </Head>
+      {/* Twitter/X */}
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:site" content="@TuUsuario" />
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={description} />
+      <meta name="twitter:image" content={img} />
+      {/* twitter:url no es necesario; usa og:url */}
+      <meta name="theme-color" content="#F1EDE5" />
+      <meta name="apple-mobile-web-app-status-bar-style" content="#F1EDE5" />
+    </Head>
             <div className={`outer-wrapper ${template}`}>
                 {children}
             </div>
