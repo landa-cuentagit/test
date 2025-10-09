9b1f112c323e5c5796dbadf3aebdbc200fba4817
Developer-gpk <dev.goplek@gmail.com>
2025-10-09 17:39:12 -0600

    Latente review

--- apps/web/pages/home.jsx
+++ apps/web/pages/home.jsx
@@ -202,14 +202,18 @@ const Home = ({ page_settings, intro, cover, section_one, destinations_section, 
             >
                 <div id='portada' />
                 <Cover template='home' cover_section={cover} />
-                <div id='section-one' />
-                <SectionOne section_one={section_one} />
-                <div id='destinations' />
-                <Destinations destinations_section={destinations_section} />
-                <div id='capabilities' />
-                <Capabilities capabilities_section={capabilities_section} />
-                <div id='about' />
-                <About about_section={about_section} />
+                <div id='section-one'>
+                    <SectionOne section_one={section_one} />
+                </div>
+                <div id='destinations'>
+                    <Destinations destinations_section={destinations_section} />
+                </div>
+                <div id='capabilities'>
+                    <Capabilities capabilities_section={capabilities_section} />
+                </div>
+                <div id='about'>
+                    <About about_section={about_section} />
+                </div>
                 <div id='contact' />
                 <div className="show-intro" onClick={handleShowIntro} />
             </Template>
